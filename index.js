729082642106d272c47b5a7f0fe60cbd3cb3c67c
lockonmaram <maramstratos@gmail.com>
2018-05-30 17:10:38 +0700

    change input from nested array to row and column

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-function checkVowels(arr){
-  const vowels = 'AIUEO';
-  var arr2 = [];
+function checkVowels(row, col){
+  const vowels = 'AIUEOaiueo  ';
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  var board = [];
+  var board2 = [];
   var vowelBLocks = 0;
 
+  for (var i = 0; i < row; i++) {
+    var hurufArr = [];
+    for (var j = 0; j < col; j++) {
+      hurufArr.push(alphabet[Math.floor(Math.random()*alphabet.length)]);
+    }
+    board.push(hurufArr);
+  }
 
-  for (var i = 0; i < arr.length; i++) {
-    arr2.push([]);
-    for (var j = 0; j < arr[i].length; j++) {
-      arr2[i].push('cons');
+  for (var i = 0; i < board.length; i++) {
+    board2.push([]);
+    for (var j = 0; j < board[i].length; j++) {
+      board2[i].push('cons');
       for (var k = 0; k < vowels.length; k++) {
-        if (arr[i][j] === vowels[k]) {
-          arr2[i].splice(j,1,'vowel')
+        if (board[i][j] === vowels[k]) {
+          board2[i].splice(j,1,'vowel')
         }
       }
     }
   }
 
-  for (var i = 0; i < arr2.length - 1; i++) {
-    for (var j = 0; j < arr2[i].length - 1; j++) {
-      if (arr2[i][j] === 'vowel'&& arr2[i][j+1] === 'vowel' && arr2[i+1][j] ==='vowel' && arr2[i+1][j+1] === 'vowel') {
+  for (var i = 0; i < board2.length - 1; i++) {
+    for (var j = 0; j < board2[i].length - 1; j++) {
+      if (board2[i][j] === 'vowel'&& board2[i][j+1] === 'vowel' && board2[i+1][j] ==='vowel' && board2[i+1][j+1] === 'vowel') {
         vowelBLocks += 1;
       }
     }
@@ -26,18 +35,4 @@ function checkVowels(arr){
   return vowelBLocks;
 }
 
-console.log(checkVowels([
-                         ['A','X','C','Y'],
-                         ['E','O','O','S'],
-                         ['I','U','I','N'],
-                         ['M','Y','O','E'],
-                         ['P','D','A','I']
-                       ]));
-
-                     /*
-                       [ [ 'o', 'x', 'x', 'x' ],
-                         [ 'o', 'o', 'o', 'x' ],
-                         [ 'o', 'o', 'o', 'x' ],
-                         [ 'x', 'x', 'o', 'o' ],
-                         [ 'x', 'x', 'o', 'o' ] ]
-                       */
+console.log(checkVowels(5,4));
